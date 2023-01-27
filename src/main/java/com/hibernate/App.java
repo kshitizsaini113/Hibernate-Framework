c1755b24499e1dc909308fcecf2ee76880512864
kshitizsaini113 <kshitizsaini.rtk@gmail.com>
2023-01-27 12:57:24 +0530

    Hibernate Configuration File

--- src/main/java/com/hibernate/App.java
+++ src/main/java/com/hibernate/App.java
@@ -1,5 +1,8 @@
 package com.hibernate;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+
+        System.out.println(sessionFactory);
+        System.out.println(sessionFactory.isOpen());
     }
 }
